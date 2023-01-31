742a222ed379918db8ff83201db3292f351a0205
FuzzyKittyz <topher.a@hotmail.com>
2023-01-31 13:07:46 -0700

    add admin auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
   def show
+    http_basic_authenticate_with name: ENV['ADMIN_USERNAME_KEY'], password: ENV['ADMIN_PASSWORD_KEY']
   end
 end
